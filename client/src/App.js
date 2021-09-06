72a5c5c50ea329663f1056f38c4cb3a8e431e456
A1ina42 <87566639+A1ina42@users.noreply.github.com>
2021-09-06 21:18:25 +0300

    add styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyle />
 			<Pages />
 		</div>
 	);
